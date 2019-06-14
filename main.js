59bb70cef789f578bf16a2122142a770fdf464e8
Gituser011 <50904990+Gituser011@users.noreply.github.com>
2019-06-14 03:18:08 +0200

    final

--- main.js
+++ main.js
@@ -181,3 +181,42 @@ function generateQuiz(otazky, quizPole, odpovedePole, tlacidlo){
     }
 
 }
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsidXNlciIsIm1haWwiLCJzdG9yZSJdLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiNWM2MTBiNDlmZGE5ZGIyYzA4MGJmNDgzIiwiZXhwIjoxNTg2NjI4NTUwLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDYxLyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwNjEvIn0.sb_ybk24nafWoCSj9m3_pJ6FSxz0xf68E_exSZXRY2U';
+
+$(document).ready(function () {
+  $("form").submit(handleSubmit);
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  var msg = $('#msg').val();
+  var email = $('#email').val();
+
+  var data = {
+    to: "tomas.filicko@gmail.com",
+    subject: "CV - Správa",
+    body: msg,
+    from: email
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "https://stored.azurewebsites.net/api/mail",
+    headers: {
+      "Authorization": 'Bearer ' + token
+    },
+    data: JSON.stringify(data),
+    contentType: "application/json"
+  })
+    .done(function () {
+      document.getElementById("sprava1").innerHTML="Ďakujem za Vašu spätnú väzbu."
+      document.getElementById("sprava2").innerHTML=""
+      
+    })
+    .fail(function (error) {
+      document.getElementById("sprava2").innerHTML="Váš e-mail sa nepodarilo odoslať. Nevyplnili ste kolonku Správa."
+      document.getElementById("sprava1").innerHTML=""
+      
+    });
+}
